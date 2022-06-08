fff2713eebd6aab831fd79b73af7a01046338ffe
Sur0vy <sur0vy@yandex.ru>
2022-06-08 22:27:48 +0300

    сделано создание БД

--- go.mod
+++ go.mod
@@ -16,6 +16,14 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
@@ -27,7 +35,7 @@ require (
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- internal/storage/DBStorage.go
+++ internal/storage/DBStorage.go
@@ -2,21 +2,24 @@ package storage
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/Sur0vy/cows_health.git/internal/logger"
+	"time"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 type DBStorage struct {
-	//userStorage users.UserStorage
-	//db          *sql.DB
+	db *sql.DB
 }
 
-func NewDBStorage(_ context.Context, DSN string) Storage {
+func NewDBStorage(ctx context.Context, DSN string) Storage {
 	s := &DBStorage{}
-	//s.Connect()
-	//s.CreateTables(ctx)
-	//s.userStorage = users.NewDBUserStorage(s.db)
+	s.Connect(DSN)
+	s.CreateTables(ctx)
 	return s
 }
 
@@ -91,4 +94,140 @@ func (s *DBStorage) DeleteCows(ctx context.Context, IDs []int) error {
 
 func (s *DBStorage) AddCow(ctx context.Context, cow Cow) error {
 	return nil
+}
+
+func (s *DBStorage) Connect(DSN string) {
+	var err error
+	s.db, err = sql.Open("pgx", DSN)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *DBStorage) CreateTables(ctx context.Context) {
+	ctxIn, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	//1. user table
+	sqlStr := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
+		"(%s serial UNIQUE PRIMARY KEY, %s TEXT UNIQUE NOT NULL, %s TEXT NOT NULL)",
+		TUser, FUserID, FLogin, FPassword)
+	_, err := s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating table %s", TUser)
+	}
+	logger.Wr.Info().Msgf("Table created: %s", TUser)
+
+	//2. breed table
+	sqlStr = fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
+		"(%s serial UNIQUE PRIMARY KEY, %s TEXT NOT NULL)",
+		TBreed, FBreedID, FBreed)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating table %s", TBreed)
+	}
+	logger.Wr.Info().Msgf("Table created: %s", TBreed)
+
+	//3. farm table
+	sqlStr = fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
+		"(%s serial UNIQUE PRIMARY KEY, %s TEXT NOT NULL, "+
+		"%s TEXT UNIQUE NOT NULL, %s INTEGER NOT NULL)",
+		TFarm, FFarmID, FName, FAddress, FUserID)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating table %s", TFarm)
+	}
+	logger.Wr.Info().Msgf("Table created: %s", TFarm)
+
+	//4. health table
+	sqlStr = fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
+		"(%s INTEGER UNIQUE PRIMARY KEY, %s TEXT, "+
+		"%s TEXT, %s TEXT, %s TIMESTAMP)",
+		THealth, FCowID, FDrink, FStress, FIll, FUpdatedAt)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating table %s", THealth)
+	}
+	logger.Wr.Info().Msgf("Table created: %s", THealth)
+
+	//5. monitoring data table
+	sqlStr = fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s "+
+		"(%s serial UNIQUE PRIMARY KEY, %s INTEGER NOT NULL, "+
+		"%s timestamp, %s FLOAT, %s FLOAT, %s FLOAT, %s FLOAT)",
+		TMonitoringData, FMDID, FCowID, FAddedAt, FPH, FTemperature, FMovement, FCharge)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating table %s", TMonitoringData)
+	}
+	logger.Wr.Info().Msgf("Table created: %s", TMonitoringData)
+
+	//6. cow table (проверить, есть ли тип такой)
+	sqlStr = fmt.Sprintf("DROP TYPE IF EXISTS bolus_type; "+
+		"CREATE TYPE bolus_type AS ENUM ('С датчиком PH', 'Без датчика PH'); "+
+		"CREATE TABLE IF NOT EXISTS %s "+
+		"(%s serial UNIQUE PRIMARY KEY, %s TEXT NOT NULL, "+
+		"%s INTEGER NOT NULL, %s INTEGER NOT NULL, "+
+		"%s INTEGER UNIQUE NOT NULL, %s DATE NOT NULL, "+
+		"%s TIMESTAMP NOT NULL, %s bolus_type NOT NULL)",
+		TCow, FCowID, FName, FBreedID, FFarmID, FBolus, FDateOfBorn, FAddedAt, FBolusType)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating table %s", TCow)
+	}
+	logger.Wr.Info().Msgf("Table created: %s", TCow)
+
+	//links
+	//user-farm link
+	sqlStr = fmt.Sprintf("ALTER TABLE %s DROP CONSTRAINT IF EXISTS fk_user_farm; "+
+		"ALTER TABLE %s ADD CONSTRAINT fk_user_farm FOREIGN KEY (%s) REFERENCES %s (%s)",
+		TFarm, TFarm, FUserID, TUser, FUserID)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating foreign key  %s <-> %s", TFarm, TUser)
+	}
+	logger.Wr.Info().Msgf("foreign key created: %s <-> %s", TFarm, TUser)
+
+	//cow-breed link
+	sqlStr = fmt.Sprintf("ALTER TABLE %s DROP CONSTRAINT IF EXISTS fk_cow_breed; "+
+		"ALTER TABLE %s ADD CONSTRAINT fk_cow_breed FOREIGN KEY (%s) REFERENCES %s (%s)",
+		TCow, TCow, FBreedID, TBreed, FBreedID)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating foreign key  %s <-> %s", TCow, TBreed)
+	}
+	logger.Wr.Info().Msgf("foreign key created: %s <-> %s", TCow, TBreed)
+
+	//cow-farm link
+	sqlStr = fmt.Sprintf("ALTER TABLE %s DROP CONSTRAINT IF EXISTS fk_cow_farm; "+
+		"ALTER TABLE %s ADD CONSTRAINT fk_cow_farm FOREIGN KEY (%s) REFERENCES %s (%s)",
+		TCow, TCow, FFarmID, TFarm, FFarmID)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating foreign key  %s <-> %s", TCow, TFarm)
+	}
+	logger.Wr.Info().Msgf("foreign key created: %s <-> %s", TCow, TFarm)
+
+	//health-cow link
+	sqlStr = fmt.Sprintf("ALTER TABLE %s DROP CONSTRAINT IF EXISTS fk_cow_health; "+
+		"ALTER TABLE %s ADD CONSTRAINT fk_cow_health FOREIGN KEY (%s) REFERENCES %s (%s)",
+		THealth, THealth, FCowID, TCow, FCowID)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating foreign key  %s <-> %s", THealth, TCow)
+	}
+	logger.Wr.Info().Msgf("foreign key created: %s <-> %s", THealth, TCow)
+
+	//monitoring data-cow link
+	sqlStr = fmt.Sprintf("ALTER TABLE %s DROP CONSTRAINT IF EXISTS fk_cow_md; "+
+		"ALTER TABLE %s ADD CONSTRAINT fk_cow_md FOREIGN KEY (%s) REFERENCES %s (%s)",
+		TMonitoringData, TMonitoringData, FCowID, TCow, FCowID)
+	_, err = s.db.ExecContext(ctxIn, sqlStr)
+	if err != nil {
+		logger.Wr.Panic().Err(err).Msgf("Fail then creating foreign key  %s <-> %s", TMonitoringData, TCow)
+	}
+	logger.Wr.Info().Msgf("foreign key created: %s <-> %s", TMonitoringData, TCow)
+}
+
+func (s *DBStorage) Ping() error {
+	return s.db.Ping()
 }
